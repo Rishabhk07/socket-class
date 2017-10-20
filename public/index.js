4fe3a686db9562fb7b9eb75fb473e792d901c8c3
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-20 22:46:28 +0530

    basic sound working
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ public/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by rishabhkhanna on 20/10/17.
+ */
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 /**
  * Created by rishabhkhanna on 20/10/17.
  */
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req,res) =>{
+
+});
+
+app.listen('9889',()=>{
+    console.log("Magic Started")    
+})
