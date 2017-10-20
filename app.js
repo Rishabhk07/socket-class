f3693ae44f2781ecc6f71aae67ac21cfbe8aa203
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-21 01:01:46 +0530

    socket server working just fine
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 let allSockets = [];
+let first = 0;
+let lastPlayed = 0;
+let lastPosition = 0;
 app.use(express.static(path.join(__dirname, "public")));
 app.get('/',(req,res)=>{
    res.sendFile(path.join(__dirname, "/public/player.html"))
@@ -17,15 +20,29 @@ app.get('/start',(req,res)=>{
 })
 
 io.on('connect',(socket)=>{
-   console.log("Socket connected");
+   console.log("Socket connected " + socket.id);
    allSockets.push(socket.id);
 
     socket.on('random',()=>{
-       console.log("Random Clicked")
-        // if(allSockets[socket.id] !== null){
-        //    allSockets[socket.id] = null;
-        // }
-        socket.broadcast.emit('play','play');
+       console.log("Random Clicked" + allSockets.indexOf(socket.id));
+       console.log("Socket ID : " + socket.id);
+       if(allSockets.indexOf(socket.id) !== -1){
+           console.log("remove index" + socket.id);
+           let index = allSockets.indexOf(socket.id);
+           allSockets.splice(index,1);
+       }
+        let randomIndex = Math.floor(Math.random() * allSockets.length);
+       let id = allSockets[randomIndex];
+       console.log("id: " + id);
+       console.log("all " + allSockets);
+       first = 1;
+       if(first !== 0 && lastPlayed !== 0 ){
+           io.to(lastPlayed).emit('getPosition',(function (data) {
+               lastPosition = data;
+           }))
+       }
+        lastPlayed = id;
+       io.to(id).emit('play',`{position : ${lastPosition} }`);
     });
 
     socket.on('together',()=>{
@@ -34,6 +51,18 @@ io.on('connect',(socket)=>{
         // }
         socket.broadcast.emit('play','play');
     });
+
+    socket.on('stop', ()=>{
+        console.log(allSockets);
+        socket.broadcast.emit('stop','stop');
+    })
+
+    socket.on('disconnect',()=>{
+        console.log("Disconected");
+        let index = allSockets.indexOf(socket.id);
+        allSockets.splice(index,1);
+        console.log(allSockets);
+    })
 
 });
 
